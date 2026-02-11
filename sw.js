a6059b9ddfc4202f696e5b7cca9813d3e48f36a3
gabect <100373972+gabect@users.noreply.github.com>
2026-02-11 11:59:15 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,24 +1,24 @@
-const CACHE="dospad-v1";
-const FILES=["./","./index.html","./manifest.webmanifest"];
+const CACHE = "dospad-cache-v2";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest"
+];
 
-self.addEventListener("install",e=>{
-  e.waitUntil(
-    caches.open(CACHE).then(c=>c.addAll(FILES))
-  );
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
   self.skipWaiting();
 });
 
-self.addEventListener("activate",e=>{
-  e.waitUntil(
-    caches.keys().then(keys=>
-      Promise.all(keys.map(k=>k!==CACHE?caches.delete(k):null))
-    )
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => (k === CACHE ? null : caches.delete(k)))))
   );
   self.clients.claim();
 });
 
-self.addEventListener("fetch",e=>{
-  e.respondWith(
-    caches.match(e.request).then(r=>r||fetch(e.request))
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request))
   );
 });
